1aa17f90eb5113ca468aa1a5c6cd58fde3c32011
tcyang1971 <tcyang@gm.pu.edu.tw>
2025-09-07 13:17:06 +0800

    第六次commit (狀態管理)

--- app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
@@ -12,6 +12,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,7 +39,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Play(modifier: Modifier = Modifier) {
-    var lucky = (1..100).random()
+    var lucky by remember { mutableStateOf((1..100).random()) }
 
     Column(modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
